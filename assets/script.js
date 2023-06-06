2ffab5a1126f177bb32678031763f8e3c19f950c
robstowe <robstowe94@gmail.com>
2023-06-06 06:23:46 -0700

    updated the portfolio

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+var craftURL = document.querySelector('#craftevents');
+
+function urlupdate(){
+    var targetURL = document.createElement('a');
+    craftURL.setAttribute('herf', targetURL);
+    craftURL.setAttribute('target', '_blank');
+
+}
+
+addEventListener
